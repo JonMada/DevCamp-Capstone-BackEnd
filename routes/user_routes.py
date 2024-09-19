81a6f791495287eb46c94b2a5a2e024a7251be8a
JonMada <jonmadariaga93@gmail.com>
2024-09-19 16:46:36 +0200

    username update

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -28,14 +28,16 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/token", response_model=dict)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == form_data.username).first()
+    user = db.query(User).filter(User.username == form_data.username).first()
+    
     if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(data={"sub": user.email})
+        
+    access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
 @router.get("/me/", response_model=UserSchema)
