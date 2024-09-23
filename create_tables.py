43751ea63c1f75bc7e9989851c46c2d17e359b16
JonMada <jonmadariaga93@gmail.com>
2024-09-23 13:19:33 +0200

    Binary

--- create_tables.py
+++ create_tables.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Large
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.orm import sessionmaker
 
-# Configuración de la base de datos
+
 DATABASE_URL = "postgresql://etxelit_db_user:6o3QBA70ph1xhIf2169I7C8n01xdDOZP@dpg-crlfv4t6l47c7382a8ag-a.oregon-postgres.render.com/etxelit_db"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
-# Definición del modelo User
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
@@ -15,26 +15,26 @@ class User(Base):
     email = Column(String(100), unique=True, index=True)
     password_hash = Column(String(255))
     
-    # Relación con el modelo Book
+    
     books = relationship("Book", back_populates="owner")
 
-# Definición del modelo Book
+
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
     author = Column(String(255), nullable=False)
     year_published = Column(Integer)
-    cover_image = Column(LargeBinary)
+    cover_image = Column(Text)
     summary = Column(Text)
     review = Column(Text)
     rating = Column(Integer)
     owner_id = Column(Integer, ForeignKey('users.id'))
     
-    # Relación con el modelo User
+    
     owner = relationship("User", back_populates="books")
 
-# Crear todas las tablas en la base de datos
+
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
