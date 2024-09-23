00a970275c50994701d7f0383e2375632b454447
JonMada <jonmadariaga93@gmail.com>
2024-09-23 10:42:25 +0200

    Get current user - auth

--- auth.py
+++ auth.py
@@ -59,12 +59,11 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         if payload is None:
             raise credentials_exception
         
-     
-        email: str = payload.get("sub") 
-        if email is None:
+        username: str = payload.get("sub")  
+        if username is None:
             raise credentials_exception
         
-        user = db.query(UserModel).filter(UserModel.email == email).first()
+        user = db.query(UserModel).filter(UserModel.username == username).first()  
         if user is None:
             raise credentials_exception
     except JWTError:
